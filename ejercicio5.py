f2ab480e07b551d515bc3328513e2e9d167c8ea8
COMPU <jessicatituana.vvc.1a@gmail.com>
2022-03-27 01:08:36 -0500

    first commit

---
+++ ejercicio5.py
@@ -0,0 +1,3 @@
+"""5) Realiza un programa que pida al usuario un número entero del 0 al 9, y que mientras el número no sea correcto se repita el proceso. Luego debe comprobar si el número se encuentra en la lista de números y notificarlo:
+
+Consejo: La sintaxis "valor in lista" permite comprobar fácilmente si un valor se encuentra en una lista (devuelve True o False)"""
