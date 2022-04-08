44e384406c26b66472d5536ae9f4368d6d7c7a7f
05029jessi <85355705+05029jessi@users.noreply.github.com>
2022-04-08 13:59:01 -0500

    Update ejercicio8.py

--- ejercicio8.py
+++ ejercicio8.py
@@ -2,3 +2,12 @@
 Crear una función recursiva que permita calcular el factorial de un número. 
 Realiza un programa principal donde se lea un entero y se muestre el resultado del factorial."""
 
+num = int(input("Ingresar un número"))
+
+def factorial(num):
+    print "Valor inicial ->",num 
+    if numero > 1:
+        num= num*factorial(num-1)
+    print "valor final ->",num
+    return numero
+print("El factorial de",num,"es", num)
