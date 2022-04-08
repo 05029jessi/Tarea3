a8bb3e51ec2d2c8ac6c68f485ddb22c4fcec481b
COMPU <jessicatituana.vvc.1a@gmail.com>
2022-04-08 10:30:08 -0500

    nuevo

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,2 +1,16 @@
 """Ejercicio 2
 Crea un programa que pida dos número enteros al usuario y diga si alguno de ellos es múltiplo del otro. Crea una función EsMultiplo que reciba los dos números, y devuelve si el primero es múltiplo del segundo."""
+
+num1 = int(input("Ingresar el primer número"))
+num2 = int(input("Ingresar el segundo número"))
+
+
+def EsMultiplo(num1, num2):
+    if num1 % num2 == 0:
+        mult = True
+        print(num1, "es multiplo de", num2)
+    else:
+        mult = False
+        print(num1, "no es multiplo de", num2)
+
+EsMultiplo(num1, num2)
--- ejercicio3.py
+++ ejercicio3.py
@@ -1,2 +1,10 @@
 """Ejercicio 3
 Crear una función que calcule la temperatura media de un día a partir de la temperatura máxima y mínima. Crear un programa principal, que utilizando la función anterior, vaya pidiendo la temperatura máxima y mínima de cada día y vaya mostrando la media. El programa pedirá el número de días que se van a introducir."""
+
+max = int(input("Ingresar la temperatura máxima"))
+min = int(input("Ingresar la temperatura mínima"))
+
+def temp_media(max, min):
+    temp_media = (max+min)/2 
+    print("Siendo la temperatura máxima",max,"y la temperatura mínima", min,"su tamperatura media es: ",temp_media)
+temp_media(max, min)
