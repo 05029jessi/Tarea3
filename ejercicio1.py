04a9fd9ef824d3ee7bace0e40d50090b806d00db
COMPU <jessicatituana.vvc.1a@gmail.com>
2022-04-08 22:31:37 -0500

    m

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,10 +1,19 @@
 """Ejercicio 1
-Crea un función EscribirCentrado, que reciba como parámetro un texto y lo escriba centrado en pantalla 
-(suponiendo una anchura de 80 columnas; pista: deberás escribir 40 - longitud/2 espacios antes del texto).
+Crea un función EscribirCentrado, que reciba como parámetro un text y lo escriba centrado en pantalla 
+(suponiendo una anchura de 80 columnas; pista: deberás escribir 40 - longitud/2 separacions antes del text).
  Además subraya el mensaje utilizando el carácter =."""
 
-texto = input("Ingrese un texto")
+def EscribirCentrado(column,text):
+    separacion=int(column/2)-(int(len(text)/2))
 
-def EscribirCentrado(texto):
+    for i in range(0,separacion):
+        print(end =" ")
+    print(text)
 
-EscribirCentrado(texto)
+    for j in range(0,separacion):
+        print(end =" ")
+
+    for k in range(0,len(text)):
+        print(end ="=")
+
+EscribirCentrado(80,"Curso Python Marzo 2022")
--- ejercicio5.py
+++ ejercicio5.py
@@ -1,5 +1,7 @@
 """Ejercicio 5
-Crea una función “calcularMaxMin” que recibe una lista con valores numéricos y devuelve el valor máximo y el mínimo. Crea un programa que pida números por teclado y muestre el máximo y el mínimo, utilizando la función anterior."""
+Crea una función “calcularMaxMin” que recibe una lista con valores numéricos y devuelve el valor máximo 
+y el mínimo. Crea un programa que pida números por teclado y muestre el máximo y el mínimo, utilizando 
+la función anterior."""
 
 def calcularManMin(val, tam, max, min):
     
