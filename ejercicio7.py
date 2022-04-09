0a90d1f9eaaf551542849504eb4fb399c6f1473d
COMPU <jessicatituana.vvc.1a@gmail.com>
2022-04-08 23:50:34 -0500

    edit

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,8 +1,32 @@
 """Ejercicio 7
-Crear una subrutina llamada “Login”, que recibe un nombre de usuario y una contraseña y 
+Crear una subrutina llamada “login”, que recibe un nombre de usuario y una contraseña y 
 te devuelve Verdadero si el nombre de usuario es “usuario1” y la contraseña es “asdasd”. 
 Además recibe el número de intentos que se ha intentado hacer login y si no se ha podido hacer 
 login incremente este valor.
 
 Crear un programa principal donde se pida un nombre de usuario y una contraseña y se 
 intente hacer login, solamente tenemos tres oportunidades para intentarlo."""
+
+def login (user,password,n):
+    if user == "usuario1" and password == "asdasd": 
+        n=0
+        return True
+    else:
+        n+=1
+        return False
+
+def main():
+    global n
+    n=0
+    while n<3:
+        usuario=str(input("Usuario: "))
+        clave=str(input("Contraseña: "))
+        if n<3 and login(usuario,clave,n)==True:
+            print("Acceso permitido.")
+            pass
+        else:
+            print("El usuario o la clave son incorrectos.")
+            n+=1
+    if n==3:
+        print("Ha completado el  número máximo de intentos.")
+main()
--- ejercicio9.py
+++ ejercicio9.py
@@ -5,3 +5,26 @@ Se divide el número mayor entre el menor.
 Si la división es exacta, el divisor es el MCD.
 Si la división no es exacta, dividimos el divisor entre el resto obtenido y se continúa de esta forma hasta obtener una división exacta, siendo el último divisor el MCD.
 Crea un programa principal que lea dos números enteros y muestre el MCD."""
+
+def Metodo_Euclides(num1,num2):
+
+    if num1>num2:
+        while num1%num2!=0:
+            r=num1%num2
+            num1=num2
+            num2=r
+        return num2
+    elif num1<num2:
+        while num2%num1!=0:
+            r=num2%num1
+            num2=num1
+            num1=r
+        return num1
+    else:
+        return num2
+        
+def main():
+    print("*MCD*\n Ingresar dos números enteros para calcular su MCD:")
+    num1=int(input());num2=int(input())
+    print("MCD:",Metodo_Euclides(num1,num2))
+main()
