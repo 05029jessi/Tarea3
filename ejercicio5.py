e599bb0464980aa64c69dcf21cbe44245c56b4a3
COMPU <jessicatituana.vvc.1a@gmail.com>
2022-04-08 22:56:19 -0500

    edit

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,8 +1,20 @@
 """Ejercicio 5
 Crea una función “calcularMaxMin” que recibe una lista con valores numéricos y devuelve el valor máximo 
 y el mínimo. Crea un programa que pida números por teclado y muestre el máximo y el mínimo, utilizando 
-la función anterior."""
+la función anterior.""" 
 
-def calcularManMin(val, tam, max, min):
-    
-calcularManMin(val, tam, max, min)
+def calcularMaxMin(lista_num):
+        maximo=max(lista_num)
+        minimo=min(lista_num)
+        print("El valor máximo es:",maximo)
+        print("El  valor mínimo es:",minimo)
+
+def main():
+    n1=int(input("Ingresar una cierta cantidad de valores: "))
+    lista=[]
+    print("Ingresar los valores: ")
+    for i in range(0,n1):
+        n2=int(input())
+        lista.append(n2)
+    calcularMaxMin(lista)
+main()
